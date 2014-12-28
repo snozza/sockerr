82aafb426fd7f48464238fd8cb8d9fec9737f491
Andrew Snead <andrewcsnead@gmail.com>
2014-12-28 12:38:25 +1100

    first 2 tests passing

--- app.js
+++ app.js
@@ -9,4 +9,19 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var Schema = mongoose.Schema;
 
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('index')
+});
+
+module.exports = app;
+if(!module.parent) {
+  app.listen(3000, function() {
+    console.log('listening on 3000');
+  })
+};
--- test/features/homepage.js
+++ test/features/homepage.js
@@ -1,7 +1,13 @@
-casper.test.begin('assertSelectorHasText() tests', 1, function(test) {
+casper.test.begin('assertSelectorHasText() tests', 2, function(test) {
     casper.start('http://localhost:3000', function() {
         test.assertSelectorHasText('title', 'Chitter');
-    }).run(function() {
-        test.done();
+    })
+
+    casper.then(function() {
+      test.assertSelectorHasText('h1', 'Chitter');
+    });
+
+    casper.run(function() {
+      test.done();
     });
 });
