f2e9aa4ea2ff7bfddfd55c69b7fc3936cb3afac4
Andrew Snead <andrewcsnead@gmail.com>
2014-12-31 01:28:22 +1100

    separated out jquery into interface script

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,7 +1,44 @@
-function init() {
+function loadPosts() {
   $.get('http://localhost:3000/posts', function(data) {
-  console.log(data);
+      $.each(data.reverse(), function(index, post) {
+          console.log(post)
+          $('.post-post').append('<li class="post-body">' + post.body + '<p class="post-user">' + post.createdAt + '</p></li>').fadeIn('slow');
+      });
   });
-}
+}; 
 
-init()
+$(document).ready(function() {
+
+  $('#submit').on('click', function(event) {
+      event.preventDefault();
+      var body = $('#body').val();
+      console.log(body);
+      $.post('http://localhost:3000/posts', {post: body}, function(data) {
+          $('<li class="post-body">' + data.body + '<p class="post-user">' + data.createdAt + '</p></li>').hide().prependTo('.post-post').fadeIn('slow');
+      });
+  });
+
+  $("#login").on('click', function(event) {
+      event.preventDefault();
+      var email = $('#email').val();
+      var password = $('#password').val();
+      $.ajax({
+          type: "POST",
+          url: "/sessions",
+          data: {"email": email, "password":password},
+          cache: false,
+          success: function(result) {
+              var page = $(location).attr('href');
+              if(result == 'correct') {
+                  $('.form-signin').fadeOut('slow', function() {
+                      $(document.body).load(page).fadeIn('slow');
+                  });
+              }
+              else {
+                  console.log('fail');
+                  return false;
+              }
+          }
+      }) 
+  });
+});
