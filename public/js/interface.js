25c9eadc831c10e74ed0c72de42128ef609e386a
Andrew Snead <andrewcsnead@gmail.com>
2014-12-31 12:51:15 +1100

    reorganised interface jquery functions

--- public/js/interface.js
+++ public/js/interface.js
@@ -5,40 +5,48 @@ function loadPosts() {
           $('.post-post').append('<li class="post-body">' + post.body + '<p class="post-user">' + post.createdAt + '</p></li>').fadeIn('slow');
       });
   });
-}; 
+}
+
+function makePost() {
+  var body = $('#body').val();
+      console.log(body);
+      $.post('http://localhost:3000/posts', {post: body}, function(data) {
+          $('<li class="post-body">' + data.body + '<p class="post-user">' + data.createdAt + '</p></li>').hide().prependTo('.post-post').fadeIn('slow');
+      });
+}
+
+function validLogin() {
+  var email = $('#email').val();
+  var password = $('#password').val();
+  $.ajax({
+    type: "POST",
+    url: "/sessions",
+    data: {"email": email, "password":password},
+    cache: false,
+    success: function(result) {
+      var page = $(location).attr('href');
+      if(result == 'correct') {
+          $('.form-signin').fadeOut('slow', function() {
+              $(document.body).load(page).fadeIn('slow');
+          });
+      }
+      else {
+          console.log('fail');
+          return false;
+      }
+    }
+  });
+}
 
 $(document).ready(function() {
 
   $('#submit').on('click', function(event) {
       event.preventDefault();
-      var body = $('#body').val();
-      console.log(body);
-      $.post('http://localhost:3000/posts', {post: body}, function(data) {
-          $('<li class="post-body">' + data.body + '<p class="post-user">' + data.createdAt + '</p></li>').hide().prependTo('.post-post').fadeIn('slow');
-      });
+      makePost();
   });
-
+      
   $("#login").on('click', function(event) {
       event.preventDefault();
-      var email = $('#email').val();
-      var password = $('#password').val();
-      $.ajax({
-          type: "POST",
-          url: "/sessions",
-          data: {"email": email, "password":password},
-          cache: false,
-          success: function(result) {
-              var page = $(location).attr('href');
-              if(result == 'correct') {
-                  $('.form-signin').fadeOut('slow', function() {
-                      $(document.body).load(page).fadeIn('slow');
-                  });
-              }
-              else {
-                  console.log('fail');
-                  return false;
-              }
-          }
-      }) 
+      validLogin();       
   });
 });
