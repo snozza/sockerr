20337a472a6adbaa6ed321d5bddf9ae8f8cfa1c6
Andrew Snead <andrewcsnead@gmail.com>
2015-01-14 10:27:59 +0000

    first jax tests passing

--- test/example.js
+++ test/example.js
@@ -1,10 +1,14 @@
-var webdriverio = require('webdriverio'),
-    expect      = require('chai').expect;
+var webdriverio = require('webdriverio');
+var expect = require('chai').expect;
+var User = require('../lib/models/User');
 
 describe('Main page tests', function(){
 
+
+
     this.timeout(99999999);
     var client = {};
+    var waitFor;
 
     before(function(done){
       new User({username: "test", email: "test@test.com",
@@ -22,10 +26,24 @@ describe('Main page tests', function(){
           expect(err).to.eql(undefined);
           expect(title).to.eql('Sockerr');
         })
+        .element('.post-post', function(err, post) {
+          expect(post).to.not.exist
+        })
+       
         .call(done);
     });
 
     it('should be able to login', function(done) {
+      client
+        .url('http://localhost:3000')
+        .setValue('#email', 'test@test.com')
+        .setValue('#password', 'test')
+        .click('#login')
+        .waitForExist('.post-post', 1000, function(err, post) {
+          expect(post).to.be.true;
+        })
+        .call(done);     
+    })
 
 
     after(function(done) {
