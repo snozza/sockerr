159e7af4aacb9daed7c24203f9a38902be50f685
Andrew Snead <andrewcsnead@gmail.com>
2015-02-20 10:50:20 +0000

    changing test verbiage

--- test/homepageUnitTest.js
+++ test/homepageUnitTest.js
@@ -5,14 +5,14 @@ var dbCleaner = require('./helpers/dbCleaner')
 
 describe('Working Homepage', function() {
 
-  it('should have a success status code for homepage', function(done) {
+  it('has a success status code for homepage', function(done) {
     http.get('http://localhost:3000', function(res) {
       expect(res.statusCode).to.eql(200);
       done();
     });
   });
 
-  it('should have an error status for incorrect routes', function(done) {
+  it('has an error status for incorrect routes', function(done) {
     http.get('http://localhost:3000/foo', function(res) {
       expect(res.statusCode).to.eql(404);
       done();
